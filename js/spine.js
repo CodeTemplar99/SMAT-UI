65ea2fdb9ce33e77a9fbe5f20747699f8b71687a
CodeTemplar99 <udoagwac@gmail.com>
2020-10-29 07:11:38 +0100

    toggle auth

--- js/spine.js
+++ js/spine.js
@@ -10,7 +10,9 @@ new Vue({
     methods: {
         to_login: function () {
             if (this.sign_up_con = true) {
+                this.sign_up_btn = true;
                 this.login_con = true;
+                this.login_btn = false;
                 this.sign_up_con = false;
             }
             
@@ -18,10 +20,10 @@ new Vue({
 
         to_sign_up: function () {
             if (this.login_con = true) {
-                this.sign_up_btn = true;
-                this.sign_up_con = true;
+                 this.sign_up_btn = false;
                 this.login_con = false;
-                this.login_btn = false;
+                this.login_btn = true;
+                this.sign_up_con = true;
             }
             
         }

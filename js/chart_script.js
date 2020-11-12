48f0171a1858dd8a843f543765c1c98996208665
Codetemplar99 <udoagwac@gmail.com>
2020-11-12 11:05:13 +0100

    chart script colors

--- js/chart_script.js
+++ js/chart_script.js
@@ -4,13 +4,13 @@ type: 'pie',
 data: {
 labels: ["Number of new users", "Videos sent in the last 24hrs", "Videos sent this month"],
 datasets: [{
-data: [550, 2424, 14040], // Specify the data values arr
+data: [550, 2424, 14040], // Specify the data values array
 borderColor: ['#2196f38c', '#f443368c', '#0096889'],
-backgroundColor: ['#2196f38c', '#f443368c', '#009'],
+backgroundColor: ['#2196f38c', '#17a2b8', '#2f55d4'],
 borderWidth: 1 // Specify bar border width
 }]},
 options: {
 responsive: true, // Instruct chart js to respond nicely.
-maintainAspectRatio: false, // Add to prevent default behaviour of f
+maintainAspectRatio: false, // Add to prevent default behaviour
 }
 });

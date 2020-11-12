e38a27dc7764befca8fc191db6217631f04d0d26
Codetemplar99 <udoagwac@gmail.com>
2020-11-12 10:59:53 +0100

    chart script adjust

--- js/chart_script.js
+++ js/chart_script.js
@@ -2,11 +2,11 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
 type: 'pie',
 data: {
-labels: ["Tokyo", "Mumbai", "Mexico City", "Shanghai"],
+labels: ["Number of new users", "Videos sent in the last 24hrs", "Videos sent this month"],
 datasets: [{
-data: [500, 50, 2424, 14040], // Specify the data values arr
-borderColor: ['#2196f38c', '#f443368c', '#3f51b570', '#0096889'],
-backgroundColor: ['#2196f38c', '#f443368c', '#3f51b570', '#009'],
+data: [550, 2424, 14040], // Specify the data values arr
+borderColor: ['#2196f38c', '#f443368c', '#0096889'],
+backgroundColor: ['#2196f38c', '#f443368c', '#009'],
 borderWidth: 1 // Specify bar border width
 }]},
 options: {
